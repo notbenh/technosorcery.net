82b9252e3df105bb81d1b1197d3fd0247c436da3
Jacob Helwig <jacob.helwig@gmail.com>
2010-04-04 12:19:45 -0700

    Run linkchecker after building or publishing to make sure we don't have any malformed/broken links.

--- publish.zsh
+++ publish.zsh
@@ -4,6 +4,7 @@ typeset -A opts
 zparseopts -D -A opts -- -debug -dry-run -publish
 
 debug=''
+linkchecker_url='http://localhost'
 if (( ${+opts[--debug]} )); then
     debug='--debug'
 fi
@@ -16,5 +17,20 @@ find build -type f -exec chmod a+r  {} \;
 if (( ${+opts[--publish]} )); then
     dry_run=''
     if (( ${+opts[--dry-run]} )); then dry_run='--dry-run'; fi
-    rsync $dry_run -vrlptSPhi --copy-unsafe-links --delete-after --delay-updates --exclude="*.tbz" --stats build/. technosorcery.net:technosorcery.net/
+    rsync $dry_run -vrlptSPhi \
+        --copy-unsafe-links \
+        --delete-after \
+        --delay-updates \
+        --exclude="*.tbz" \
+        --stats \
+        build/. \
+        technosorcery.net:technosorcery.net/
+    linkchecker_url='http://technosorcery.net'
 fi
+
+linkchecker --priority \
+    -a \
+    --ignore-url='http://[^/]*.w3.org/.*/?check/referer' \
+    --ignore-url='http://git.kernel.org/\?p=.*' \
+    --ignore-url='http://mephistoblog.com/' \
+    $linkchecker_url
